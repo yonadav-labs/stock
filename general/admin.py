1efbc4a78d1553a38c18cc72a12046fa8af42738
yonadav-labs <yonadav.barilan.upwork@gmail.com>
2017-11-28 11:47:08 -0600

    format time on admin

--- general/admin.py
+++ general/admin.py
@@ -6,8 +6,11 @@ from django.contrib import admin
 from general.models import *
 
 class OutPutAdmin(admin.ModelAdmin):
-    list_display = ['symbol', 'shares_max', 'shares_min', 'discount', 'ft_type', 'ma_type', 'duration', 'timestamp']
+    list_display = ['symbol', 'shares_max', 'shares_min', 'discount', 'ft_type', 'ma_type', 'duration', 'formatted_time']
     search_fields = ['name']
+
+    def formatted_time(self, obj):
+    	return obj.timestamp.strftime("%Y-%m-%d %H:%M:%S")
 
 admin.site.register(StockInput)
 admin.site.register(StockOutput, OutPutAdmin)
