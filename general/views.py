3b1e4158258ecb56cf695223a39403f0e3bb00b8
yonadav-labs <yonadav.barilan.upwork@gmail.com>
2017-12-14 15:29:48 -0600

    add confirm modal

--- general/views.py
+++ general/views.py
@@ -23,11 +23,11 @@ def i_post(request, symbol):
 
         if form.is_valid():
             form.save()
-    else:        
-        form = OutputForm(initial={
-            'symbol': symbol,
-            'min_price': 0
-        })
+            
+    form = OutputForm(initial={
+        'symbol': symbol,
+        'min_price': 0
+    })
 
     return render(request, 'i_post.html', {
         'form': form,
