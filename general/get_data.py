4b002da0c50ac74459c3af72a476ea4899f5978c
yonadav-labs <yonadav.barilan.upwork@gmail.com>
2017-12-25 16:09:52 -0600

    finish get history

--- general/get_data.py
+++ general/get_data.py
@@ -4,6 +4,7 @@ import urllib2
 
 from lxml import html
 from os import sys, path
+from datetime import datetime
 
 sys.path.append(path.dirname(path.dirname(path.abspath(__file__))))
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "stock.settings")
@@ -26,11 +27,20 @@ def get_history(issue):
     url = 'https://finance.yahoo.com/quote/{}/history'.format(issue.symbol.upper())
     content = urllib2.urlopen(url).read()
     tree = html.fromstring(content)
-    print content
-    # trs = tree.xpath('//*[@id="Col1-1-HistoricalDataTable-Proxy"]/section/div[2]/table')
-    # print len(trs)
+    
+    trs = tree.xpath('//*[@data-test="historical-prices"]/tbody/tr')
+    for tr in trs:
+        _, created = PriceHistory.objects.get_or_create(symbol=issue.symbol,
+                                                        open=tr.xpath('./td[2]/span/text()')[0],
+                                                        high=tr.xpath('./td[3]/span/text()')[0],
+                                                        low=tr.xpath('./td[4]/span/text()')[0],
+                                                        close=tr.xpath('./td[5]/span/text()')[0],
+                                                        date=datetime.strptime(tr.xpath('./td[1]/span/text()')[0], '%b %d, %Y').strftime('%Y-%m-%d'))
+
+        if not created:
+            break
 
 if __name__ == '__main__':
-    for ii in IssueTable.objects.all()[1:]:
-        # get_issue(ii)
+    for ii in IssueTable.objects.all():
+        get_issue(ii)
         get_history(ii)
